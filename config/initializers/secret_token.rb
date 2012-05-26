4ca282b57e47b5717c208805250e143a98166d5c
Sonakshi Bajaj <sonakshibajaj@gmail.com>
2012-05-26 14:47:14 +0530

    first commit - 26-05-12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autobots::Application.config.secret_token = 'cd0767c6008a2a96026badf0f1ed03416198362a61f192a10454f699846d357056626e47fc7d404d5a38af846be753a6b50fc3a05683eaa2c9fe5f6311ca19e7'
