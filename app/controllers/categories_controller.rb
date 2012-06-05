af5f52cfb4e1b50c3ff5a64d158139f8de10855d
Sonakshi Bajaj <sonakshibajaj@gmail.com>
2012-06-05 21:23:19 +0530

    Created the show and add category functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,28 @@
 class CategoriesController < ApplicationController
+
   def new
-  	@category = Category.new
-  	render :show
+		@category = Category.new
   end
 
   def create
+		@category = Category.new(params[:category])
+		if @category.save
+			redirect_to @category		
+		else
+			render :index
+		end
   end
 
   def show
+		@category = Category.find(params[:id])
+		
   end
 
   def destroy
   end
 
 	def index
-		@category = Category.all
+		@category = Category.new
 	end
 
 end
